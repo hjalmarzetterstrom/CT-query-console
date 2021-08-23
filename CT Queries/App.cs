b321b714cb106840a14bd7afbca60e5572311d80
Hjalmar Zetterström <hjalmar.zetterstrom@avensia.com>
2021-08-23 10:56:16 +0200

    indentation

--- CT Queries/App.cs
+++ CT Queries/App.cs
@@ -5,29 +5,29 @@ using System.Threading.Tasks;
 
 public class App
 {
-	private readonly IClient _client;
-	public App(IClient client) { _client = client; }
+    private readonly IClient _client;
+    public App(IClient client) { _client = client; }
 
-	public async Task Run()
-	{
-		while (true)
-		{
-			await AddTestNote();
+    public async Task Run()
+    {
+        while (true)
+        {
+            await AddTestNote();
 
-			Console.ReadKey();
-			Console.Clear();
-		}
-	}
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
 
-	private async Task AddTestNote()
-	{
-		var customer = await _client.Builder().Customers().GetById("12332-CUST0M3R-GU1D-ID-ETC").ExecuteAsync();
-		await _client.Builder().Customers().UpdateById(customer).AddAction(() =>
-			new commercetools.Sdk.Domain.Customers.UpdateActions.SetCustomFieldUpdateAction
-			{
-				Name = "Notes",
-				Value = new[] { "Test note" }
-			}
-			).ExecuteAsync();
-	}
+    private async Task AddTestNote()
+    {
+        var customer = await _client.Builder().Customers().GetById("12332-CUST0M3R-GU1D-ID-ETC").ExecuteAsync();
+        await _client.Builder().Customers().UpdateById(customer).AddAction(() =>
+            new commercetools.Sdk.Domain.Customers.UpdateActions.SetCustomFieldUpdateAction
+            {
+                Name = "Notes",
+                Value = new[] { "Test note" }
+            }
+            ).ExecuteAsync();
+    }
 }
