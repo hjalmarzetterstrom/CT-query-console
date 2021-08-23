97548be3cb8620e398809b4009708568b2cfc8be
Hjalmar Zetterström <hjalmar.zetterstrom@avensia.com>
2021-08-23 10:53:13 +0200

    Add example query

--- CT Queries/App.cs
+++ CT Queries/App.cs
@@ -1,21 +1,33 @@
 ﻿using commercetools.Sdk.Client;
+using commercetools.Sdk.HttpApi.CommandBuilders;
 using System;
 using System.Threading.Tasks;
 
 public class App
 {
-    private readonly IClient _client;
-    public App(IClient client) { _client = client; }
+	private readonly IClient _client;
+	public App(IClient client) { _client = client; }
 
-    public async Task Run()
-    {
-        while (true)
-        {
+	public async Task Run()
+	{
+		while (true)
+		{
+			await AddTestNote();
 
-            // Use _client to query CT here.
+			Console.ReadKey();
+			Console.Clear();
+		}
+	}
 
-            Console.ReadKey();
-            Console.Clear();
-        }
-    }
+	private async Task AddTestNote()
+	{
+		var customer = await _client.Builder().Customers().GetById("12332-CUST0M3R-GU1D-ID-ETC").ExecuteAsync();
+		await _client.Builder().Customers().UpdateById(customer).AddAction(() =>
+			new commercetools.Sdk.Domain.Customers.UpdateActions.SetCustomFieldUpdateAction
+			{
+				Name = "Notes",
+				Value = new[] { "Test note" }
+			}
+			).ExecuteAsync();
+	}
 }
