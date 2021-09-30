8879b5c581a993e89fca2f2b08fb174d42a4765a
Hjalmar Zetterström <hjalmar.zetterstrom@avensia.com>
2021-09-30 12:38:10 +0200

    Add method extensions

--- CT Queries/App.cs
+++ CT Queries/App.cs
@@ -1,7 +1,20 @@
 ﻿using commercetools.Sdk.Client;
+using commercetools.Sdk.Domain.Predicates;
+using commercetools.Sdk.Domain.Query;
+using commercetools.Sdk.Domain.ShoppingLists;
 using commercetools.Sdk.HttpApi.CommandBuilders;
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using commercetools.Sdk.Domain;
+using commercetools.Sdk.Domain.Customers;
+using commercetools.Sdk.Domain.Customers.UpdateActions;
+using System.Globalization;
+using commercetools.Sdk.Domain.Carts.UpdateActions;
+using commercetools.Sdk.Domain.Products.UpdateActions;
+using commercetools.Sdk.Domain.Types.UpdateActions;
+using commercetools.Sdk.Domain.Subscriptions;
 
 public class App
 {
@@ -29,5 +42,48 @@ public class App
                 Value = new[] { "Test note" }
             }
             ).ExecuteAsync();
+    }
+}
+
+public static class Extensions
+{
+    public static TValue GetValue<TValue>(this CustomFields customFields, string fieldName, TValue defaultValue = default)
+    {
+        var field = customFields?.Fields.SingleOrDefault(f => f.Key == fieldName);
+        if (field is null || field.Equals(default(KeyValuePair<string, object>)))
+            return defaultValue;
+
+        if (field.Value is TValue value)
+            return value;
+
+        if (field.Value.Value is TValue innerValue)
+            return innerValue;
+
+        // When you have a FieldSet without any values the commercetools SDK won't know which the generic type is so we need
+        // some fiddling to return the correct type
+        if (field.Value.Value is FieldSet<object> genericFieldSet && !genericFieldSet.Any() && typeof(TValue).IsGenericType && typeof(FieldSet<>) == typeof(TValue).GetGenericTypeDefinition())
+            return Activator.CreateInstance<TValue>();
+
+        try
+        {
+            return (TValue)Convert.ChangeType(field.Value.Value, typeof(TValue), CultureInfo.InvariantCulture);
+        }
+        catch (Exception e)
+        {
+            throw new ArgumentException($"The Commercetools custom field {fieldName} is of type {field.Value.GetType().FullName} but tried to access it as {typeof(TValue).FullName}", e);
+        }
+    }
+
+    public static LocalizedString ToLocalizedString(this Dictionary<string, string> dictionary)
+    {
+        if (dictionary is null)
+            return null;
+
+        var localizedString = new LocalizedString();
+        foreach (var kvp in dictionary)
+        {
+            localizedString[kvp.Key] = kvp.Value;
+        }
+        return localizedString;
     }
 }
